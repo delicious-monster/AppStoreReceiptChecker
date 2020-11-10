2d044ab0c298b7da330ff4870f2fdb223057f4f6
Maxim Ananov <pointum@gmail.com>
2020-11-10 17:31:57 +0300

    Require macOS 10.12 in SwiftPM too

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "AppStoreReceiptChecker",
     platforms: [
-        .macOS(.v10_11)
+        .macOS(.v10_12)
     ],
     products: [
         .library(name: "AppStoreReceiptChecker", targets: ["AppStoreReceiptChecker"]),
